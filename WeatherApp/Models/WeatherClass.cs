6b6052adc8d73c3981b7aaaea464b641d0abd6d4
dmeraz <darin.meraz@gmail.com>
2016-10-27 00:21:43 -0400

    first commit, completed tutorial, added more humidity and wind speed to WeatherOrNot View, customized Shared Layout

---
+++ WeatherApp/Models/WeatherClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    public class WeatherClass
+    {
+        public Object getWeatherForecast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=dcc54514a9fb87e33b7c1ebf436bc9a4&units=imperial";
+            //synchronous client
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
